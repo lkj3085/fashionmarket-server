76777bc4a125c0d9ab845acc6062f6342a38c1de
lkj3085 <lkj3085@gmail.com>
2021-08-23 20:19:56 +0900

    fashion market server

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var hostname = '127.0.0.1';
+var port = 8080;
+
+const server = http.createServer(function (req, res) {
+    const path = req.url;
+    const method = req.method;
+    if(path === '/products'){
+        if(method === 'GET'){
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            const products = JSON.stringify([{
+                name: "댄디남 셔츠",
+                price: 30000
+            }
+        ]);
+        res.end(products);
+    }else if (method === 'post'){
+        res.end('생성 완료');
+    }
+}
+res.end('Bye');
+});
+
+server.listen(port, hostname);
+
+console.log("fashtion market server on");
